d6fe03469e2dab9192c109bafcf8efce445ff32a
bessyhuang <bessyhuang1@gmail.com>
2021-04-08 11:57:06 +0800

    Function: Query expansion, Search FJU books, wikipedia summary

--- LIBchatbot_app/views.py
+++ LIBchatbot_app/views.py
@@ -11,12 +11,14 @@ import requests
 from bs4 import BeautifulSoup
 from googlesearch import search
 
-from collections import defaultdict
-from collections import Counter
+from collections import defaultdict, Counter
 from math import log, sqrt
+from opencc import OpenCC
 import pandas as pd
+import wptools
+import pickle
 import sys
-sys.path.insert(0, 'C:/Users/bessyhuang/Desktop/django_linebot_files/LIBchatbot_app')
+sys.path.insert(0, '/home/bessyhuang/AI-Chatbot/LIBchatbot_app')
 import pymongo_custom_module as PymongoCM
 import text_preprocess as tp
 
@@ -25,23 +27,24 @@ line_bot_api = LineBotApi(settings.LINE_CHANNEL_ACCESS_TOKEN)
 parser = WebhookParser(settings.LINE_CHANNEL_SECRET)
 
 # ------------------------------------------------------------------------------------------
-Question_list, Q_WS_list, Answer_list, new_Cluster_list, AllField_list = PymongoCM.get_mongodb_row("Library", "FAQ")
+Question_list, Q_WS_list, A_WS_list, Answer_list, new_Cluster_list, AllField_list = PymongoCM.get_mongodb_row("Library", "FAQ")
 FAQ_df = pd.DataFrame({"content": Q_WS_list, "new_Cluster": new_Cluster_list, "answer": Answer_list})
 FAQ_df["clean_msg"] = FAQ_df.content.apply(tp.text_process)
 
+# ------------------------------------------------------------------------------------------
 zipped = zip(Q_WS_list, new_Cluster_list)
 Cluster_set = set(new_Cluster_list)
 
 def SegList_ClusterGroup(zipped):
-	Cluster_dict = defaultdict(list)
+    Cluster_dict = defaultdict(list)
 	
-	for row_Q, row_c in zipped:
-		if row_c in Cluster_dict:
-			Cluster_dict[row_c] += row_Q
-		else:
-			Cluster_dict[row_c] = row_Q
-	# print(Cluster_dict['Cluster 1'])
-	return Cluster_dict
+    for row_Q, row_c in zipped:
+        if row_c in Cluster_dict:
+            Cluster_dict[row_c] += row_Q
+        else:
+            Cluster_dict[row_c] = row_Q
+    # print(Cluster_dict['Cluster 1'])
+    return Cluster_dict
 
 SegString_Cluster_dict = SegList_ClusterGroup(zipped)
 Q = SegString_Cluster_dict.values()
@@ -55,148 +58,171 @@ Q_clean_list = FAQ_cluster_df.clean_msg.tolist()
 processed_docs = Q_clean_list
 
 # --------------------------------------------------------------------------------------------
-### total_tokens 尚未正規化字彙的出現頻率
+# ====== total_tokens 尚未正規化字彙的出現頻率 ======
 NO_norm_vocab_freqs = []
 no_norm_doc_list = []
 for no_norm_doc in Q_WS_list:
-	no_norm_doc_list += no_norm_doc
+    no_norm_doc_list += no_norm_doc
 NO_norm_vocab_freqs.append(Counter(no_norm_doc_list))
-# print(NO_norm_vocab_freqs)
-
 total_tokens = len(NO_norm_vocab_freqs[0])
-# print(total_tokens)
 
 
-### 建立 vocab_freqs (正規化字彙的出現頻率)
+# ====== 建立 vocab_freqs (正規化字彙的出現頻率) ======
 vocab_freqs = []
 norm_doc_str = ""
 for norm_doc in processed_docs:
-	norm_doc_str += norm_doc + " "
+    norm_doc_str += norm_doc + " "
 vocab_freqs.append(Counter(norm_doc_str.split()))
-#print(vocab_freqs)
+total_vocabs = len(vocab_freqs[0])
 
 
-### 建立 vocab (正規化字彙的索引)
+# ====== 建立 vocab (正規化字彙的索引) ======
 vocab = defaultdict(int)
 for term in norm_doc_str.split():
-	if term not in vocab:
-		vocab[term] = len(vocab)
-# print('~~~', vocab["借書"], vocab)
-# print(len(vocab_freqs[0]))
+    if term not in vocab:
+        vocab[term] = len(vocab)
 
 
-### doc_term_freqs 儲存每個文件分別的字彙及詞頻Counter
-doc_term_freqs = []
-for norm_doc in processed_docs:
-	# print(list(norm_doc.split()), type(norm_doc))
-	norm_doc = list(norm_doc.split())
-
-	# 計算詞頻
-	tfs = Counter(norm_doc)
-	doc_term_freqs.append(tfs)
-# print(len(doc_term_freqs))
-# print('+++---+++', doc_term_freqs[0])
+# ====== doc_TF 儲存每個文件分別的字彙及詞頻Counter ======
+doc_TF = []
+for norm_doc_STRING in processed_docs:
+    norm_doc = list(norm_doc_STRING.split())
+    TF = Counter(norm_doc) # 計算詞頻
+    #TF_Output: Counter({'館際合作': 2, '回覆': 2, '時間': 2, '多久': 2}) -> One document
+    doc_TF.append(TF)
 
 
 class InvertedIndex:
-	def __init__(self, vocab, doc_term_freqs):
-		# vocab：正規化字彙的索引
-		self.vocab = vocab
-		# doc_len：每個文件的長度
-		self.doc_len = [0] * len(doc_term_freqs)
-		# doc_term_freqs：儲存這個 doc 中相對應 doc_ids (doc_vocab) 的詞頻。各個詞出現在每個文件的詞頻
-		self.doc_term_freqs = [[] for i in range(len(vocab))]
-		# doc_ids：儲存這個 doc 所包含的 所有字彙(doc_vocab) 的 ID
-		self.doc_ids = [[] for i in range(len(vocab))]
-		# doc_freqs：各個詞總共出現在多少個文件中
-		self.doc_freqs = [0] * len(vocab)
-		# total_num_docs：總共處理的文件數量
-		self.total_num_docs = 0
-		# max_doc_len：單一文件最長的長度
-		self.max_doc_len = 0
+    def __init__(self, vocab, doc_TF):
+        self.vocab = vocab
+        self.vocab_len = len(vocab)
+        self.doc_len = [0] * len(doc_TF)
+        self.doc_TF = [[] for i in range(len(vocab))]
+        self.doc_ids = [[] for i in range(len(vocab))]
+        self.doc_freqs = [0] * len(vocab)
+        self.total_num_docs = 0
+        self.max_doc_len = 0   #Longest Document Length
+        
+        for docid, term_freqs in enumerate(doc_TF):            
+            doc_len = sum(term_freqs.values())            
+            self.max_doc_len = max(doc_len, self.max_doc_len)
+            self.doc_len[docid] = doc_len            
+            self.total_num_docs += 1
+            for term, freq in term_freqs.items():
+                term_ID = vocab[term]
+                self.doc_ids[term_ID].append(docid)
+                self.doc_TF[term_ID].append(freq)                
+                self.doc_freqs[term_ID] += 1
+                
+    def num_docs(self):
+        return self.total_num_docs
 
-		for docid, term_freqs in enumerate(doc_term_freqs):
-			doc_len = sum(term_freqs.values())
-			self.max_doc_len = max(doc_len, self.max_doc_len) ###
-			self.doc_len[docid] = doc_len
-			self.total_num_docs += 1
-			for term, freq in term_freqs.items():
-				term_id = vocab[term]
-				self.doc_ids[term_id].append(docid)
-				self.doc_term_freqs[term_id].append(freq)
-				self.doc_freqs[term_id] += 1
+    def term_show_in_docids(self, term):
+        term_ID = self.vocab[term]
+        return self.doc_ids[term_ID]
 
-	def num_terms(self):
-		return len(self.doc_ids)
+    def term_in_each_doc_TermFreqs(self, term):
+        term_id = self.vocab[term]
+        return self.doc_TF[term_id]
 
-	def num_docs(self):
-		return self.total_num_docs
+    def term_show_in_N_docs(self, term): 
+        term_ID = self.vocab[term]
+        return self.doc_freqs[term_ID]
 
-	def docids(self, term):
-		term_id = self.vocab[term]
-		return self.doc_ids[term_id]
+INV_INDEX = InvertedIndex(vocab, doc_TF)
 
-	def freqs(self, term):
-		term_id = self.vocab[term]
-		return self.doc_term_freqs[term_id] # doc_term_freqs：儲存這個 doc 中相對應 doc_ids (doc_vocab) 的詞頻。各個詞出現在每個文件的詞頻
+# ======== TF-IDF ========
+def query_tfidf(query, invindex, k=5):
+    scores = Counter()   # scores 儲存了 docID 和他們的 TF-IDF分數
+    N = invindex.num_docs() # Cluster -1 ~ Cluster 530 => 532 個
+    query_vector = []
+    query_term = []
+    
+    for term in query:
+        if term in vocab:
+            term_show_in_N_docs = invindex.term_show_in_N_docs(term)
+            query_idf = log(N / term_show_in_N_docs)
+            query_vector.append(query_idf)
+            query_term.append(term)
+        else:
+            query_vector.append(0)
+            query_term.append(term)
 
-	def f_t(self, term): 
-		term_id = self.vocab[term]
-		return self.doc_freqs[term_id] # doc_freqs：各個詞總共出現在多少個文件中
+    for term in query:
+        i = 0
+        term_show_in_N_docs = invindex.term_show_in_N_docs(term)
+        for docid in invindex.term_show_in_docids(term):
+            term_in_each_doc_TermFreqs = invindex.term_in_each_doc_TermFreqs(term)[i]     
+            term_Maxfreqs_in_doc = max(invindex.term_in_each_doc_TermFreqs(term))
 
-	def space_in_bytes(self):
-		# 我們假設每個integer使用8 bytes
-		space_usage = 0
-		for doc_list in self.doc_ids:
-			space_usage += len(doc_list) * 8
-		for freq_list in self.doc_term_freqs:
-			space_usage += len(freq_list) * 8
-		return space_usage
+            doc_len = invindex.doc_len[docid] #每個 doc 的長度
+            tfidf_cal = log(1 + term_in_each_doc_TermFreqs) * log(N / term_show_in_N_docs) / sqrt(doc_len)
 
-# print("Number of documents = {}".format(len(processed_docs)))
-# print("Number of unique terms = {}".format(len(vocab)))
-# print("Number of tokens = {}".format(total_tokens))
-
-# print("--- --- ---")
-invindex = InvertedIndex(vocab, doc_term_freqs)
-
-# print inverted index stats
-# print("documents = {}".format(invindex.num_docs()))
-# print("number of terms = {}".format(invindex.num_terms()))
-# print("longest document length = {}".format(invindex.max_doc_len))
-# print("uncompressed space usage MiB = {:.3f}".format(invindex.space_in_bytes() / (1024.0 * 1024.0)))
-
-# 給定一個查詢(String)和一個索引(Class)，回傳k個文件
-def query_tfidf(query, index, k=10):
-	# scores 儲存了docID和他們的TF-IDF分數
-	scores = Counter()
-	N = index.num_docs()
-	
-	for term in query:
-		i = 0
-		f_t = index.f_t(term)
-		for docid in index.docids(term):
-			# f_(d,t)
-			f_d_t = index.freqs(term)[i]
-			d = index.doc_len[docid]
-			tfidf_cal = log(1+f_d_t) * log(N/f_t) / sqrt(d)
-			scores[docid] += tfidf_cal
-			i += 1
-	
-	return scores.most_common(k)
-
+            scores[docid] += tfidf_cal
+            i += 1
+    return scores.most_common(k)
+    
+    
+# 問題
 Q_list = []
 for doc in FAQ_cluster_df.content:
-	Q_list.append(''.join(doc))
+    Q_list.append(''.join(doc))
 
+# 回答
 A_list = []
 sectors = FAQ_df.groupby('new_Cluster')
 sectors_len = len(sectors)
 for ClusterN in range(-1, sectors_len -1, 1):
     ClusterN_index = list(sectors.get_group('Cluster {}'.format(ClusterN)).index)[0]
-    # print(FAQ_df.loc[ClusterN_index].answer)
     A_list.append(FAQ_df.loc[ClusterN_index].answer)
 
+
+# ----- wikipedia 擴展關鍵詞 ---------------------------------
+with open('./LIBchatbot_app/food_dict.pkl', 'rb') as fp:
+    wiki_food_dict = pickle.load(fp)
+fp.close()
+
+
+subCategory_dict = defaultdict(str)
+for key, subcat_items in wiki_food_dict.items():
+    for item in subcat_items:
+        subCategory_dict[item] = key
+# print(subCategory_dict)
+
+
+wiki_GroupCategory_list = []
+for key in wiki_food_dict.keys():
+    wiki_GroupCategory_list.append(wiki_food_dict[key] + [key])
+# print('wiki_category_list = ', wiki_GroupCategory_list)
+
+
+total_wiki = []
+for i in wiki_GroupCategory_list:
+    total_wiki += i
+# print('total_wiki = ', total_wiki)
+
+
+custom_match_dict = {
+    '零食':'食物', '飲料':'飲料', 
+    '系統':'電腦', '借閱證':'閱覽證', 
+    '團討室':'團體 討論室', '互借':'館際 互借', 
+    '智慧財產權':'智財權'
+    } # wiki_category : FAQ_vocab 
+# ------------------------------------------------------------
+# ----- 查詢館藏的停用詞擷取 -----------------------------------
+cluster529_stopwords = set()
+Cluster529 = list(sectors.get_group('Cluster 529').content)[0]
+for ws in Cluster529:
+    cluster529_stopwords.add(ws)
+# print('***', cluster529_stopwords)
+# ------------------------------------------------------------
+# ----- 查詢Wikipedia的停用詞擷取 ------------------------------
+cluster530_stopwords = set()
+Cluster530 = list(sectors.get_group('Cluster 530').content)[0]
+for ws in Cluster530:
+    cluster530_stopwords.add(ws)
+# print('***', cluster530_stopwords)
+# ------------------------------------------------------------
 
 def Keyword_filter(stemmed_query):
 	query_keyword = []
@@ -210,75 +236,103 @@ def Keyword_filter(stemmed_query):
 
 @csrf_exempt
 def callback(request):
+    if request.method == 'POST':
+        signature = request.META['HTTP_X_LINE_SIGNATURE']
+        body = request.body.decode('utf-8')
  
-	if request.method == 'POST':
-		signature = request.META['HTTP_X_LINE_SIGNATURE']
-		body = request.body.decode('utf-8')
+        try:
+            events = parser.parse(body, signature)
+        except InvalidSignatureError:
+            return HttpResponseForbidden()
+        except LineBotApiError:
+            return HttpResponseBadRequest()
  
-		try:
-			events = parser.parse(body, signature)  # 傳入的事件
-		except InvalidSignatureError:
-			return HttpResponseForbidden()
-		except LineBotApiError:
-			return HttpResponseBadRequest()
- 
-		for event in events:
-			if isinstance(event, MessageEvent):  # 如果有訊息事件
-				
-				# 查詢語句
-				stemmed_query = event.message.text.split()
+        for event in events:
+            if isinstance(event, MessageEvent):  # 如果有訊息事件
+                # query預處理，為了讓查詢跟索引內容相同
+                query = event.message.text.split()
+                clean_query = list(''.join(tp.text_process(query)).split())
+                print(clean_query)
+                print('---------------------------\n')
+                
+                # ----- wikipedia 擴展關鍵詞 -------------------------------------
+                final_query = []
+                for w in clean_query:
+                    if (w in total_wiki) and (w not in vocab.keys()):
+                        # e.g. 品客、零食
+                        wikiCategory_term = subCategory_dict[w]
+                        query_term = custom_match_dict[wikiCategory_term]
 
-				query_keyword = Keyword_filter(stemmed_query)
-				if query_keyword == []:
-					msg_list = []
-					results = query_tfidf(stemmed_query, invindex)
+                        final_query.append(query_term)
 
-					count = 0
-					for rank, res in enumerate(results):
-						if count < 5:
-							print("排名 {:2d} DOCID {:8d} ClusterN {:8d} SCORE {:.3f} \n內容 {:}".format(rank+1, res[0], res[0]-1, res[1], Q_list[res[0]][:50]))
-							msg = [Q_list[res[0]], A_list[res[0]]]
-							msg_list.append(msg)
-							count += 1
-						else:
-							pass
+                    elif (w in total_wiki) and (w in vocab.keys()):
+                        # e.g. 飲料、食物
+                        final_query.append(w)
 
-					print(msg_list)
-					for i, j in msg_list:
-						print(i, j)
+                    elif w in custom_match_dict.keys():
+                        query_term = custom_match_dict[w]
+                        if ' ' in query_term:
+                            query_list = query_term.split()
+                            for i in query_list:
+                                final_query.append(i)
+                        else:
+                            final_query.append(query_term)
 
-					line_bot_api.reply_message(
-						event.reply_token,[
-							TextSendMessage(text=content) for score, content in msg_list
-						])
+                    else:
+                        # 沒有在 FAQ ，也沒有在 wiki            
+                        final_query.append(w)
 
-				else:
-					# Wiki
-					msg_list = []
-					Keyword = ' '.join(query_keyword)
-					print(Keyword)
-					url = 'https://zh.wikipedia.org/wiki/' + Keyword #'https://www.google.com/search?q={}'.format(Keyword)
-					response = requests.get(url)
+                print('Final_query =', final_query)
+                print('--------------------------------------------------------')
+                
+                results = query_tfidf(final_query, INV_INDEX)
+                msg_list = []
+                for rank, res in enumerate(results):
+                    print("排名 {:2d} DOCID {:8d} ClusterN {:8d} SCORE {:.3f} \n內容 {:}\n".format(rank+1, res[0], res[0]-1, res[1], Q_list[res[0]][:50]))
+                    
+                    if res[1] > 1:
+                        # ----- 查詢館藏的關鍵字擷取 -----------------------------------
+                        if res[0] - 1 == 529:
+                            search_FJULIB_KEYWORD = ""
+                            for w in final_query: 
+                                if w not in cluster529_stopwords:
+                                    search_FJULIB_KEYWORD = w
+                    
+                            print('\n查詢館藏的關鍵字擷取：', search_FJULIB_KEYWORD)
+                            raw_res = A_list[res[0]]
+                            final_res = raw_res + search_FJULIB_KEYWORD
+                            msg = [Q_list[res[0]], final_res]
+                            msg_list.append(msg)
 
-					wiki_msg = [Keyword, response.url]
-					msg_list.append(wiki_msg)
-					# line_bot_api.reply_message(  # 回復傳入的訊息文字
-					#	 event.reply_token,
-					#	 TextSendMessage(text=wiki_msg)
-					# )
+                        # ----- 查詢Wikipedia的停用詞擷取 ------------------------------
+                        elif res[0] - 1 == 530:
+                            search_WIKI_KEYWORD = ""
+                            for w in final_query: 
+                                if w not in cluster530_stopwords:
+                                    search_WIKI_KEYWORD = w
+                    
+                            print('\n查詢 Wikipedia 的關鍵字擷取：', search_WIKI_KEYWORD)
+                            raw_res = A_list[res[0]]
+                
+                
+                            # 摘要
+                            page1 = wptools.page(search_WIKI_KEYWORD, lang='zh').get_restbase('/page/summary/')
+                            summary = page1.data['exrest']
+                            wikiURL = page1.data['url']
 
-					# Google
-					for url in search(Keyword, stop=2):
-						response = requests.get(url)
-						soup = BeautifulSoup(response.text, 'html.parser')
-						title = soup.find('title').text
-						msg = [title, url]
-						msg_list.append(msg)
-
-					line_bot_api.reply_message(
-						event.reply_token, [
-							TextSendMessage(text=url) for title, url in msg_list
-						])
-		return HttpResponse()
-	else:
-		return HttpResponseBadRequest()
+                            cc = OpenCC('s2twp')
+                            final_res = cc.convert(summary) + '\n' + wikiURL
+                            msg = [Q_list[res[0]], final_res]
+                            msg_list.append(msg)
+                        
+                        else:
+                            final_res = A_list[res[0]]
+                            msg = [Q_list[res[0]], final_res]
+                            msg_list.append(msg)
+                        
+                #print("排名 {:2d} DOCID {:8d} ClusterN {:8d} SCORE {:.3f} \n內容 {:}\n回覆 {:}\n".format(rank+1, res[0], res[0]-1, res[1], Q_list[res[0]][:50], final_res))
+                
+                line_bot_api.reply_message(event.reply_token, [TextSendMessage(text=a) for q, a in msg_list])
+        return HttpResponse()
+    else:
+        return HttpResponseBadRequest()
